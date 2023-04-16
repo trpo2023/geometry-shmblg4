3efca1841444e80c7cdf99c2a9a0936396a2f440
Matvey Gotfrid <matveygot@yandex.ru>
2023-04-17 03:00:56 +0700

    Add colors.h

---
+++ src/libgeometry/colors.h
@@ -0,0 +1,6 @@
+#define RED(X) "\e[1;31m" X "\e[0m"
+#define GREEN(X) "\e[0;32m" X "\e[0m"
+#define YELLOW(X) "\e[0;33m" X "\e[0m"
+#define BLUE(X) "\e[0;34m" X "\e[0m"
+#define PURPLE(X) "\e[0;35m" X "\e[0m"
+#define CYAN(X) "\e[0;36m" X "\e[0m"
